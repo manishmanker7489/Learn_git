b085f2ab62f4731cdd4ec482e327fdb87e5a4894
Manish Manker <manishmanker877@gmail.com>
2023-06-23 15:23:23 +0530

    add var c in mutiply file

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,7 @@ public class Multiply {
     public static void main(String[] args) {
         int a= 10;
         int b = 20;
-
-        System.out.println(a*b);
+        int c  = 30;
+        System.out.println(a*b * c);
     }
 }
